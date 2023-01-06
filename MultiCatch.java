2913c0c0d153a09cf3a71704f4e3517fffbfbffc
Ninad <118526559+nxinad@users.noreply.github.com>
2023-01-06 12:09:25 +0530

    Add files via upload

---
+++ MultiCatch.java
@@ -0,0 +1,28 @@
+
+class MultiCatch {
+
+	public static void main(String args[]) {
+		try {
+
+
+			int a = args.length;
+			System.out.println("a = " + a);
+
+			int b = 50 / a;
+
+			int c[] = { 1 };
+
+			c[42] = 99;
+		} 
+		catch(ArithmeticException e) {
+		System.out.println("Divide by 0: " + e);
+		} 
+
+		catch(ArrayIndexOutOfBoundsException e) {
+		System.out.println("Arrays : " + e);
+		}
+
+		System.out.println("After try/catch blocks.");
+		}
+
+}
